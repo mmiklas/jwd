1644d014126fc789661b18e5192000566ff8ee2c
miki <m.miklas@wszib.edu.pl>
2020-12-12 09:19:20 +0100

    Jeszcze jedna zmiana

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10 ; i++) {
+        for (int i = 0; i < 1000 ; i++) {
             System.out.println("Hello World222222!");
         }
 
